6a40989719ea43b551b698b73f1b8111e8558a1d
tymer007 <ty.freeman47@gmail.com>
2024-08-14 11:16:36 +0100

    update

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,114 +6,144 @@ import { faFigma, faSketch, faJs, faNode } from '@fortawesome/free-brands-svg-ic
 
 const Portfolio = () => {
     return (
-        <div className="bg-black text-white min-h-screen flex items-center py-12 px-4 space-x-4">
-            {/* Profile Section */}
-            <div className="w-1/3 max-w-2xl bg-gray-800 rounded-lg m-12 p-6 text-center mb-12">
-                <img src="path_to_image" alt="Olamide Afolabi" className="mx-auto rounded-full mb-4 w-40 h-40 object-cover" />
-                <h1 className="text-3xl font-bold mb-2">Olamide Afolabi</h1>
-                <p className="text-lg">[Graphics Designer] [UI/UX Designer]</p>
-                <p className="text-sm text-gray-400 mt-2">Passionate about crafting intuitive and engaging user experiences...</p>
+        <div className='bg-black text-white min-h-screen py-12'>
+            <div className=" flex px-4 space-x-4">
+                {/* Profile Section */}
+                <div className="w-1/3 max-w-2xl bg-white rounded-lg m-12 p-4 px-6 text-left mb-12 h-fit">
+                    <img src="jhines09-nba-6280s.JPG" alt="Olamide Afolabi" className="mx-auto rounded-xl my-4 object-cover" />
+                    <h1 className="text-3xl font-bold mb-2 text-black text-center">Olamide Afolabi</h1>
+                    <p className="text-gray-500 mt-2 w-fit text-center">A Graphics Designer and UI/UX Designer that loves to create visually appealing designs</p>
+                </div>
+
+                <div className='w-2/3'>
+
+                    <div className="w-full max-w-4xl text-start mb-12">
+                        <h2 className="text-4xl font-extrabold mt-8">Graphics Designer</h2>
+                        <h2 className="text-4xl font-extrabold text-gray-500">UI/UX Designer</h2>
+                        <p className="text-gray-400 mt-2 w-1/2">Passionate about crafting intuitive and engaging user experiences. Skilled in transforming ideas into beautifully executed products.</p>
+                    </div>
+
+                    {/* Experience Section */}
+                    <div className="w-full max-w-4xl mb-12">
+                        <div className="grid grid-cols-3 gap-4">
+                            <div className='flex flex-col'>
+                                <h2 className="text-5xl font-bold">+5</h2>
+                                <p className="text-gray-400 max-w-32 font-medium">YEARS OF EXPERIENCE IN GRAPHIC DESIGN</p>
+                            </div>
+                            <div className='flex flex-col'>
+                                <h2 className="text-5xl font-bold">1</h2>
+                                <p className="text-gray-400 max-w-32 font-medium">YEARS OF EXPERIENCE IN UI/UX DESIGN</p>
+                            </div>
+                            <div className='flex flex-col'>
+                                <h2 className="text-5xl font-bold">+150</h2>
+                                <p className="text-gray-400 max-w-32 font-medium">PROJECTS COMPLETED ALTOGETHER</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Work Section */}
+                    <div className="w-full max-w-4xl text-left mb-12">
+                        <h2 className="text-8xl font-extrabold mt-8">5 YEARS OF <br /> EXPERIENCE</h2>
+                        <div className='flex space-x-2'>
+                            <FontAwesomeIcon icon={faArrowRight} className="text-orange-500 mt-1" />
+                            <div>
+                                <h3 className="text-xl font-bold mb-2">Nhub Foundation</h3>
+                                <p className="text-lg">Junior UI/UX Designer</p>
+                                <p className="text-gray-400 mb-4">Worked with a team of UI designers...</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="w-full max-w-4xl text-left mb-12">
+                        <h2 className="text-8xl font-extrabold mt-8">A YEAR OF <br /> EXPERIENCE</h2>
+                        <div className='flex space-x-2'>
+                            <FontAwesomeIcon icon={faArrowRight} className="text-orange-500 mt-1" />
+                            <div>
+                                <h3 className="text-xl font-bold mb-2">Nhub Foundation</h3>
+                                <p className="text-lg">Junior UI/UX Designer</p>
+                                <p className="text-gray-400 mb-4">Worked with a team of UI designers...</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    {/* Featured Projects */}
+                    <div className="w-full max-w-4xl mb-12">
+                        <h2 className="text-8xl font-extrabold mt-8">FEATURED<br /> PROJECTS</h2>
+                        <h2 className="text-xl font-extrabold py-1">UI/UX DESIGNS</h2>
+                        <div className="flex flex-col">
+                            {/* Add your project details and images here */}
+                            <div className="flex gap-4 py-6">
+                                <img src="RentalDates.svg" alt="Project" className="rounded mb-2" />
+                                <div className='place-self-center'>
+                                    <h2 className="text-2xl font-extrabold py-1 hover:underline hover:text-yellow-300">RIDAL</h2>
+                                    <p className="text-gray-400">A Ride-sharing App</p>
+                                </div>
+                            </div>
+
+                            <div className="flex gap-4 py-6">
+                                <img src="RentalDates.svg" alt="Project" className="rounded mb-2" />
+                                <div className='place-self-center'>
+                                    <h2 className="text-2xl font-extrabold py-1 hover:underline hover:text-blue-950">MENTORSPACE</h2>
+                                    <p className="text-gray-400 max-w-80">An Educational Platform That Connects Students to MentorsAn Educational Platform That Connects Students to Mentors</p>
+                                </div>
+                            </div>
+
+                            <div className="flex gap-4 py-6">
+                                <img src="RentalDates.svg" alt="Project" className="rounded mb-2" />
+                                <div className='place-self-center'>
+                                    <h2 className="text-2xl font-extrabold py-1 hover:underline hover:text-slate-600">AUTOLEASE</h2>
+                                    <p className="text-gray-400">Car Rental Booking Platform</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Premium Tools */}
+                    <div className="w-full max-w-4xl mb-12">
+                        <h2 className="text-8xl font-extrabold mt-8">PREMIUM<br /> TOOLS</h2>
+                        <div className="grid grid-cols-2 gap-y-4">
+                            <FontAwesomeIcon icon={faSketch} size="3x" />
+                            <FontAwesomeIcon icon={faJs} size="3x" />
+                            <FontAwesomeIcon icon={faNode} size="3x" />
+                            <FontAwesomeIcon icon={faFigma} size="3x" />
+                        </div>
+                    </div>
+
+                    {/* Contact Section */}
+                    <div className="w-full max-w-2xl pb-2">
+                        <a href="https://www.behance.net/samuelafolabi5" target='_blank' className="inline-flex items-center">
+                            <FontAwesomeIcon icon={faBehance} size="5x" className="mr-4 bg-blue-700 px-4 rounded-xl py-4" />
+                            <h2 className="text-6xl font-extrabold mb-4 text-white hover:text-blue-700 hover:underline mt-5">BEHANCE<br /></h2>
+                        </a>
+                    </div>
+
+                    <div className="w-full max-w-2xl pb-2">
+                        <a href="https://instagram.com/_lamidesign?igshid=NTc4MTIwNjQ2YQ" target='_blank' className="inline-flex items-center hover:text-[#c13584]">
+                            <FontAwesomeIcon icon={faInstagram} size="8x" className="mr-4 bg-[#c13584] 0 px-1.5 rounded-xl text-white" />
+                            <h2 className="text-6xl font-extrabold mb-4 text-white hover:text-[#c13584] hover:underline mt-5">INSTAGRAM</h2>
+                        </a>
+                    </div>
+
+                    <div className="w-full max-w-2xl">
+                        <a href="https://wa.me/qr/L4CNWH6YAFYXN1" target='_blank' className="inline-flex items-center">
+                            <FontAwesomeIcon icon={faWhatsapp} size="8x" className="mr-4 bg-green-500 px-1.5 rounded-xl" />
+                            <h2 className="text-6xl font-extrabold text-white hover:text-green-500 hover:underline">CONTACT<br />ME HERE</h2>
+                        </a>
+                    </div>
+
+
+                    {/* Footer */}
+
+
+                </div>
+
             </div>
-
-            <div className='w-2/3'>
-
-                <div className="w-full max-w-4xl text-start mb-12">
-                    <h2 className="text-4xl font-extrabold mt-8">Graphics Designer</h2>
-                    <h2 className="text-4xl font-extrabold text-gray-500">UI/UX Designer</h2>
-                    <p className="text-sm text-gray-400 mt-2 w-1/2">Passionate about crafting intuitive and engaging user experiences. Skilled in transforming ideas into beautifully executed products.</p>
-
-                </div>
-
-                {/* Experience Section */}
-                <div className="w-full max-w-4xl text-center mb-12">
-                    <div className="grid grid-cols-3 gap-4">
-                        <div>
-                            <h2 className="text-2xl font-bold">+5</h2>
-                            <p className="text-gray-400">Years of Experience in Graphic Design</p>
-                        </div>
-                        <div>
-                            <h2 className="text-2xl font-bold">+1</h2>
-                            <p className="text-gray-400">Year of Experience in UI/UX Design</p>
-                        </div>
-                        <div>
-                            <h2 className="text-2xl font-bold">+150</h2>
-                            <p className="text-gray-400">Projects Completed Altogether</p>
-                        </div>
-                    </div>
-                    <h2 className="text-4xl font-extrabold mt-8">5 Years of Experience</h2>
-                </div>
-
-                {/* Work Section */}
-                <div className="w-full max-w-4xl text-left mb-12">
-                    <h3 className="text-xl font-bold mb-2">Freelancer</h3>
-                    <FontAwesomeIcon icon={faArrowRight} className="text-orange-500" />
-                    <p className="text-lg">Graphics Designer</p>
-                    <p className="text-gray-400 mb-4">Designed flyers, logos, brand identity, and social media ads...</p>
-                </div>
-
-                <div className="w-full max-w-4xl text-left mb-12">
-                    <FontAwesomeIcon icon={faArrowRight} className="text-orange-500" />
-
-                    <div>
-                        <h3 className="text-xl font-bold mb-2">Nhub Foundation</h3>
-                        <p className="text-lg">Junior UI/UX Designer</p>
-                        <p className="text-gray-400 mb-4">Worked with a team of UI designers...</p>
-                    </div>
-                </div>
-
-                {/* Featured Projects */}
-                <div className="w-full max-w-4xl mb-12">
-                    <h2 className="text-3xl font-extrabold text-center mb-8">Featured Projects</h2>
-                    <div className="grid grid-cols-3 gap-4">
-                        {/* Add your project details and images here */}
-                        <div className="text-center">
-                            <img src="path_to_project_image" alt="Project" className="rounded mb-2" />
-                            <h3 className="text-lg font-bold">Ridal</h3>
-                            <p className="text-gray-400">A Ride-sharing App</p>
-                        </div>
-                        <div className="text-center">
-                            <img src="path_to_project_image" alt="Project" className="rounded mb-2" />
-                            <h3 className="text-lg font-bold">MentorSpace</h3>
-                            <p className="text-gray-400">An Educational Platform...</p>
-                        </div>
-                        <div className="text-center">
-                            <img src="path_to_project_image" alt="Project" className="rounded mb-2" />
-                            <h3 className="text-lg font-bold">AutoLease</h3>
-                            <p className="text-gray-400">Car Rental Booking Platform</p>
-                        </div>
-                    </div>
-                </div>
-
-                {/* Premium Tools */}
-                <div className="w-full max-w-4xl mb-12">
-                    <h2 className="text-3xl font-extrabold text-center mb-8">Premium Tools</h2>
-                    <div className="grid grid-cols-4 gap-4 text-center">
-                        <FontAwesomeIcon icon={faSketch} size="3x" />
-                        <FontAwesomeIcon icon={faJs} size="3x" />
-                        <FontAwesomeIcon icon={faNode} size="3x" />
-                        <FontAwesomeIcon icon={faFigma} size="3x" />
-                    </div>
-                </div>
-
-                {/* Contact Section */}
-                <div className="w-full max-w-2xl text-center">
-                    <h2 className="text-3xl font-extrabold mb-4">Contact Me Here</h2>
-                    <a href="https://wa.me/yourwhatsappnumber" className="inline-flex items-center text-green-500">
-                        <FontAwesomeIcon icon={faWhatsapp} size="2x" className="mr-2" />
-                        <span className="underline">Contact me here</span>
-                    </a>
-                </div>
-
-
-                {/* Footer */}
-                <div className="text-gray-500 text-center mt-12">
-                    <p>&copy; 2024 | Designed Olamide Afolabi [Framer]</p>
-                </div>
-
-
+            <div className="text-gray-500 text-center mt-12 font-medium">
+                <p>&copy; 2024 | Designed Olamide Afolabi</p>
             </div>
-
         </div>
+
     );
 };
 
